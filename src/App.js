5cfe8ae8915da87e71c86754300fd258f2334649
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-28 20:31:22 +0300

    added title and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './assets/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+ class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='title'>
+          <h3>Quizlex</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
