5ac20f2398f6e8073ee197f304242c4b3dee3432
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-28 21:06:19 +0300

    DidMount component to bring data from our api(quiz service)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import './assets/style.css';
+import quizService from './quizService';
 
  class App extends Component {
+
+  state ={
+    questionBank: []
+  };
+
+  getQuestions = () =>{
+    quizService().then( question =>{
+      this.setState({
+        questionBank: question
+      });
+    });
+  };
+
+  componentDidMount(){
+    this.getQuestions();
+  }
+
   render() {
     return (
       <div className='container'>
         <div className='title'>
           <h3>Quizlex</h3>
         </div>
+        {
+          this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) =>
+            <h4>{question}</h4>
+          )
+        }
+
       </div>
     );
   }
